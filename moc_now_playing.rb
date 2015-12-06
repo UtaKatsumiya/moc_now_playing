d50e46989753c2d4a0de2bb07dfa7ad39c7643d7
UtaKatsumiya <grimoire.sic@gmail.com>
2015-12-06 16:45:33 +0900

    Create moc_now_playing.rb

---
+++ moc_now_playing.rb
@@ -0,0 +1,13 @@
+# -*-coding: utf-8 -*-
+# moc_now_playing
+# mocで再生中の楽曲情報をtweetするmikutterコマンドを追加するプラグイン
+
+Plugin.create(:moc_now_playing) {
+	command(:now_playing,
+	        condition: lambda { |opt| true},
+	        name: "なうぷれ",
+	        visible: true,
+	        role: :timeline) { |opt|
+			Service.primary.post(message: "#NowPlaying " + %x[ mocp -i | grep Artist | cut -c 9- ].chomp + " - " + %x[ mocp -i | grep SongTitle | cut -c 12- ])
+}
+}
